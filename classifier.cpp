9b8b72dc741add0667d23b4b6c57f57566c15aa6
abhimanyu1188 <157841746+abhimanyu1188@users.noreply.github.com>
2024-04-03 14:45:20 -0400

    Update classifier.cpp
    Removed the code to do the merge again

--- classifier.cpp
+++ classifier.cpp
@@ -2,12 +2,12 @@
 
 void train() {
     printf("train\n");
-    printf("The first feature is added\n");
+    
 }
 
 void predict() {
     printf("predict\n");
-    printf("The second feature is added.\n");
+    
 }
 
 int main() {
